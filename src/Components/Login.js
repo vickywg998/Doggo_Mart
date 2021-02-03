927e5e2675405a7ef554bf066a22880b24626d6b
Vicky Wong <vickywg998@hotmail.com>
2021-02-03 02:15:22 -0800

    WIP Added sign in, login functionality

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -21,7 +21,7 @@ export default function Login() {
       await login(emailRef.current.value, passwordRef.current.value);
       history.push("/profile");
     } catch {
-      setError("Failed to login");
+      setError("Failed to login, try again!");
     }
     setLoading(false);
   }
